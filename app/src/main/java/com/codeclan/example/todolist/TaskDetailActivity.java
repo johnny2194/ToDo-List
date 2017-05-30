e41cd5f0ceaaea412998cf2ea9fce6861221e7d0
Johnny Watson <johnny@streetchange.org.uk>
2017-05-30 16:35:51 +0100

    Add details and status to task details activity

--- app/src/main/java/com/codeclan/example/todolist/TaskDetailActivity.java
+++ app/src/main/java/com/codeclan/example/todolist/TaskDetailActivity.java
@@ -1,7 +1,10 @@
 package com.codeclan.example.todolist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class TaskDetailActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class TaskDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_detail);
+
+        Intent intent = getIntent();
+        Task task = (Task) intent.getSerializableExtra("Task");
+
+
+        TextView titleView = (TextView)  findViewById(R.id.title);
+        titleView.setText(task.getTitle());
+
+        CheckBox status = (CheckBox) findViewById(R.id.checkBox2);
+        status.setChecked(task.getStatus());
+
+        TextView descriptionView = (TextView)  findViewById(R.id.description);
+        descriptionView.setText(task.getDescription());
+
+
     }
+
+
 }
