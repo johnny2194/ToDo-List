0ae3cc694bc2c4f11807fe1e7396dc315ca06816
Johnny Watson <johnny@streetchange.org.uk>
2017-05-30 12:00:52 +0100

    Add icon to title bar

--- app/src/main/java/com/codeclan/example/todolist/TaskListActivity.java
+++ app/src/main/java/com/codeclan/example/todolist/TaskListActivity.java
@@ -1,5 +1,6 @@
 package com.codeclan.example.todolist;
 
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -13,13 +14,17 @@ public class TaskListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.task_list);
 
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(ResourcesCompat.getDrawable(getResources(), R.drawable.resize, null));
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
         TaskList defaultList = new TaskList();
         ArrayList<Task> list = defaultList.getList();
 
         TaskListAdapter taskAdapter = new TaskListAdapter(this, list);
 
 
-    ListView listView = (ListView) findViewById(R.id.list);
+        ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(taskAdapter);
     }
 }
